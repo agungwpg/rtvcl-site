70dac5c99599fe7c2797e841d3cdd45abeb1a3c3
rtcl <rtvcl.boss@gmail.com>
2022-10-16 18:55:06 +0700

    set max mobile layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="max-w-md mx-auto">
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
 export default MyApp;
